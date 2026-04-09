a5e54e775232bf4b02671ad04ddea05005a86897
cCalic04 <simondiazca@gmail.com>
2026-04-09 12:04:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function modo_claro() {
+   var element = document.body;
+   element.classList.toggle("modo_claro");
+}
+
+function tema_naranja() {
+    var element = document.body;
+    element.classList.toggle("tema_naranja")
+}
